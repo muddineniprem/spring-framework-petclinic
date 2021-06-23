0f2ae1f637685e43ea7db1dea56d8f729385a6ee
muddineniprem <muddineniprem@gmail.com>
2021-06-23 15:09:12 +0530

    updated the dbname in schema.sql

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+CREATE DATABASE IF NOT EXISTS petclinicauto;
 
-ALTER DATABASE petclinic
+ALTER DATABASE petclinicauto
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
